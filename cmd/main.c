a98b38da5650423ad409fb6b8e8a05bd8e299989
xiejw.io+m4max@gmail.com <xiejw.io+m4max@gmail.com>
2025-10-19 06:12:17 -0700

    [qwen3] added flag.

--- cmd/main.c
+++ cmd/main.c
@@ -4,12 +4,25 @@
 int
 main( void )
 {
-        error_t    err = OK;
+        error_t err = OK;
+        // 0.6 billion parameters
         Qwen3Model ptr;
-        err = qwen3_model_load( "weights.bin", &ptr );
-        if ( err != OK ) PANIC( "fail to load weights." );
 
-        goto cleanup;
+      "vocab_size": 151_936,     # Vocabulary size
+      "context_length": 40_960,  # Length originally used during training
+      "emb_dim": 1024,           # Embedding dimension
+      "n_heads": 16,             # Number of attention heads
+      "n_layers": 28,            # Number of layers
+      "hidden_dim": 3072,        # Size of intermediate dim in FeedForward
+      "head_dim": 128,           # Size of the heads in GQA
+      "qk_norm": True,           # Whether to normalize queries & keys in GQA
+      "n_kv_groups": 8,          # Key-Value groups for GQA
+      "rope_base": 1_000_000.0,  # The base in RoPE's "theta"
+
+        err = qwen3_model_load( "weights.bin", &ptr );
+      if ( err != OK ) PANIC( "fail to load weights." );
+
+      goto cleanup;
 
 cleanup:
         qwen3_model_unload( &ptr );
--- src/qwen3.h
+++ src/qwen3.h
@@ -3,7 +3,16 @@
 #include "types.h"
 
 typedef struct Qwen3Config {
-        int n_layer;
+        int vocab_size;      // Vocabulary size
+        int context_length;  // Length originally used during training
+        int emb_dim;         // Embedding dimension
+        int n_heads;         // Number of attention heads
+        int n_layers;        // Number of layers
+        int hidden_dim;      // Size of intermediate dim in FeedForward
+        int head_dim;        // Size of the heads in GQA
+        int qk_norm;         // Whether to normalize queries & keys (bool)
+        int n_kv_groups;     // Key-Value groups for GQA
+        f32 rope_base;       // The base in RoPE's "theta"
 } Qwen3Config;
 
 typedef struct Qwen3Model {
